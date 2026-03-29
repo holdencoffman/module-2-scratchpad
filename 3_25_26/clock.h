215f2128bdfb63347029d23011a9e5563602c07e
Holden Coffman <holdencoffman@gmail.com>
2026-03-29 09:20:03 -0400

    2nd lecture first video, timestamp 41:00

--- 3_25_26/clock.h
+++ 3_25_26/clock.h
@@ -4,29 +4,53 @@
 #include <sstream>
 #include <iomanip>
 
-class Clock
+enum timeType
+{
+   TWELVE = 12,
+   TWENTYFOUR = 24
+};
+
+enum class partType
+{
+   AM = 4,
+   PM = 8
+};
+
+const partType parts[2] = {partType::AM, partType::PM};
+const std::string partToStr[2] = {"AM", "PM"};
+const timeType formats[2] = {TWELVE, TWENTYFOUR};
+
+class clockType
 {
 public:
-    Clock(int = 0, int = 0, int = 0);
-    void setTime(int, int = 0, int = 0);
-    void setHour(int);
-    void setMinute(int);
-    void setSecond(int);
-    void getTime(int &, int &, int &) const;
-    int getHour() const;
-    int getMinute() const;
-    int getSecond() const;
-    std::string printTime() const;
-    void incrementSeconds();
-    void incrementMinutes();
-    void incrementHours();
-    bool equalTime(const Clock &) const;
-    void clockTick();
+   clockType(int, int = 0, int = 0, timeType = TWENTYFOUR, partType = partType::PM);
+   void setTime(int, int, int, timeType, partType);
+   void getTime(int&, int&, int&) const;
+   std::string printTime() const;
+   void incrementSeconds();
+   void incrementMinutes();
+   void incrementHours();
+
+   // couldn't see past here yet
+
+   void setHour(int);
+   void setMinute(int);
+   void setSecond(int);
+   void getTime(int &, int &, int &) const;
+   int getHour() const;
+   int getMinute() const;
+   int getSecond() const;
+   std::string printTime() const;
+   void incrementSeconds();
+   void incrementMinutes();
+   void incrementHours();
+   bool equalTime(const Clock &) const;
+   void clockTick();
 
 private:
-    int hr;
-    int min;
-    int sec;
+   int hr;
+   int min;
+   int sec;
 };
 
 #endif
